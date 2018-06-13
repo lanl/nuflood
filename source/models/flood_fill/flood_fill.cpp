8f402bd95aaa9212683d853e0bef2381404ddea3
Byron Tasseff <btasseff@lanl.gov>
2018-06-13 13:03:50 -0600

    Correcting negligence of corners in the flood fill algorithm

--- source/models/flood_fill/flood_fill.cpp
+++ source/models/flood_fill/flood_fill.cpp
@@ -29,6 +29,18 @@ FloodFill::FloodFill(const rapidjson::Value& root) {
 
 	w_.set_name("waterSurfaceElevation");
 
+	if (root.HasMember("seeds")) {
+		const rapidjson::Value& point_array_json = root["seeds"];
+
+		unsigned int i = 0;
+		while (i < point_array_json.Size()) {
+			double x = point_array_json[i++].GetDouble();
+			double y = point_array_json[i++].GetDouble();
+			double z = point_array_json[i++].GetDouble();
+			w_.Set(z, x, y);
+		}
+	}
+
 	if (!depth_file_.IsEmpty()) {
 		h_.Load(depth_file_);
 		w_.Add(h_);
@@ -89,7 +101,7 @@ void FloodFill::Grow(void) {
 			for (const INT_TYPE& column: it->second) {
 				const INT_TYPE row = it->first;
 				bool out_of_bounds = row == 0 || row == B_.num_rows()-1 ||
-														 column == 0 || column == B_.num_columns()-1;
+				                     column == 0 || column == B_.num_columns()-1;
 				if (out_of_bounds) {
 					continue;
 				}
@@ -156,6 +168,50 @@ void FloodFill::UpdateWetCells(void) {
 	}
 }
 
+void FloodFill::FillCorner(INT_TYPE column, INT_TYPE row) {
+	INT_TYPE i, j;
+	for (INT_TYPE m = 0; m < 4; m++) {
+		switch(m) {
+			case 0:
+				i = column + 1;
+				j = row;
+				break;
+			case 1:
+				i = column - 1;
+				j = row;
+				break;
+			case 2:
+				i = column;
+				j = row + 1;
+				break;
+			case 3:
+				i = column;
+				j = row - 1;
+				break;
+		}
+
+		if (i < 0 || i > B_.num_columns() - 1 || 
+		    j < 0 || j > B_.num_rows() - 1 ||
+			 B_.Get(i, j) == B_.nodata_value() ||
+			 h_.Get(i, j) <= 0.0) {
+			continue;
+		}
+
+		if (h_.Get(i, j) > 0.0) {
+			float h_new = w_.Get(i, j) - B_.Get(column, row);
+			h_.Set(h_new, column, row);
+			break;
+		}
+	}
+}
+
+void FloodFill::FillCorners(void) {
+	FillCorner(0, 0);
+	FillCorner(0, B_.num_rows() - 1);
+	FillCorner(B_.num_columns() - 1, 0);
+	FillCorner(B_.num_columns() - 1, B_.num_rows() - 1);
+}
+
 void FloodFill::Run(void) {
 	std::cout << "Iteration" << "\t" << "Number of seeds" << std::endl;
 	std::cout << num_iterations_ << "\t" << num_seeds_ << std::endl;
@@ -167,6 +223,7 @@ void FloodFill::Run(void) {
 		std::cout << num_iterations_ << "\t" << num_seeds_ << std::endl;
 	}
 
+	FillCorners();
 	h_.Write(output_folder_);
 }
 
--- source/models/flood_fill/flood_fill.h
+++ source/models/flood_fill/flood_fill.h
@@ -12,6 +12,8 @@ public:
 private:
 	void Grow(void);
 	void UpdateWetCells(void);
+	void FillCorner(INT_TYPE column, INT_TYPE row);
+	void FillCorners(void);
 
 	Folder output_folder_;
 	INT_TYPE num_seeds_, num_wet_;
