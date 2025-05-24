dc74c27d11d840daf2255202c93f2a23d74b53a4
Byron Tasseff <byron@tasseff.com>
2025-05-24 23:49:40 +0000

    Fix argument names to function.

--- source/common/error.h
+++ source/common/error.h
@@ -2,5 +2,5 @@
 
 #include <string>
 
-void PrintErrorAndExit(const std::string &error_message, int error_code = 1);
+void PrintErrorAndExit(const std::string &error_message, int exit_code = 1);
 void PrintWarning(const std::string &warning_message);
