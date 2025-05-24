e3a6e95c16a0cb728cfdffe7544642e13e5cab91
Byron Tasseff <byron@tasseff.com>
2025-05-24 23:02:21 +0000

    Use best practices and new formatting rules when reading JSON input files.

--- source/common/document.cpp
+++ source/common/document.cpp
@@ -1,15 +1,25 @@
 #include "document.h"
 #include "error.h"
 #include "file.h"
+#include <array>
+#include <memory>
 
-Document::Document(const File& file) {
-	FILE* p_file = fopen(file.path().c_str(), "r");
-	char buffer[65536];
-	rapidjson::FileReadStream file_read_stream(p_file, buffer, sizeof(buffer));
-	root.ParseStream<0>(file_read_stream);
-	fclose(p_file);
+Document::Document(const File &file) {
+    std::unique_ptr<FILE, decltype(&fclose)> p_file(
+        fopen(file.path().c_str(), "r"), &fclose);
 
-	if (!root.IsObject()) {
-		PrintErrorAndExit("File '" + file.path() + "' is not a valid JSON document.");
-	}
+    if (!p_file) {
+        PrintErrorAndExit("Failed to open file '" + file.path() + "'.");
+    }
+
+    constexpr size_t BUFFER_SIZE = 65536;
+    std::array<char, BUFFER_SIZE> buffer{};
+    rapidjson::FileReadStream file_read_stream(p_file.get(), buffer.data(),
+                                               buffer.size());
+    root.ParseStream<0>(file_read_stream);
+
+    if (!root.IsObject()) {
+        PrintErrorAndExit("File '" + file.path() +
+                          "' is not a valid JSON document.");
+    }
 }
