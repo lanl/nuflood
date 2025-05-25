6f920bed42550e293b9cb2a08ae6ba7ed39c77fa
Byron Tasseff <byron@tasseff.com>
2025-05-25 15:05:20 +0000

    Fix formatting of PointSource class.

--- source/common/point_source.h
+++ source/common/point_source.h
@@ -1,108 +1,108 @@
 #pragma once
 
+#include "file.h"
+#include <algorithm>
+#include <fstream>
 #include <iostream>
 #include <sstream>
-#include <fstream>
 #include <string>
-#include <algorithm>
 #include <vector>
-#include "file.h"
 
-template<class T>
-class PointSource {
-public:
-	PointSource(void) : x_(0.0), y_(0.0) {}
-	PointSource(const double x, const double y, const File& file);
-	void Load(const File& file);
-	void Scale(const T scalar);
+template <class T> class PointSource {
+  public:
+    PointSource() : x_(0.0), y_(0.0) {}
+    PointSource(double x_coord, double y_coord, const File &file);
+    void Load(const File &file);
+    void Scale(T scalar);
 
-	T interpolated_rate(const T current_time) const;
+    auto interpolated_rate(T current_time) const -> T;
 
-	void set_x(const double x) { x_ = x; }
-	void set_y(const double y) { y_ = y; }
+    void set_x(double x_coord) { x_ = x_coord; }
+    void set_y(double y_coord) { y_ = y_coord; }
 
-	double x(void) const { return x_; }
-	double y(void) const { return y_; }
+    auto x() const -> double { return x_; }
+    auto y() const -> double { return y_; }
 
-	INT_TYPE x_index(const Grid<T>& grid) const;
-	INT_TYPE y_index(const Grid<T>& grid) const;
+    auto x_index(const Grid<T> &grid) const -> INT_TYPE;
+    auto y_index(const Grid<T> &grid) const -> INT_TYPE;
 
-protected:
-	std::vector<T> time_;
-	std::vector<T> rate_;
-	double x_, y_;
+  private:
+    std::vector<T> time_;
+    std::vector<T> rate_;
+    double x_, y_;
 };
 
-template<class T>
-INT_TYPE PointSource<T>::x_index(const Grid<T>& grid) const {
-	return (INT_TYPE)grid.GetXIndex(x_);
+template <class T>
+auto PointSource<T>::x_index(const Grid<T> &grid) const -> INT_TYPE {
+    return (INT_TYPE)grid.GetXIndex(x_);
 }
 
-template<class T>
-INT_TYPE PointSource<T>::y_index(const Grid<T>& grid) const {
-	return (INT_TYPE)grid.GetYIndex(y_);
+template <class T>
+auto PointSource<T>::y_index(const Grid<T> &grid) const -> INT_TYPE {
+    return (INT_TYPE)grid.GetYIndex(y_);
 }
 
-template<class T>
-PointSource<T>::PointSource(const double x, const double y, const File& file) {
-	Load(file);
-	x_ = x;
-	y_ = y;
+template <class T>
+PointSource<T>::PointSource(double x_coord, double y_coord, const File &file)
+    : x_(x_coord), y_(y_coord) {
+    Load(file);
 }
 
-template<class T>
-void PointSource<T>::Load(const File& file) {
-	std::fstream input(file.path().c_str()); 
-	std::string line;
+template <class T> void PointSource<T>::Load(const File &file) {
+    std::fstream input(file.path().c_str());
+    std::string line;
 
-	while (std::getline(input, line)) {
-		// Remove white space from the beginning of the string.
-		line.erase(line.begin(), std::find_if(line.begin(), line.end(), 
-		std::not1(std::ptr_fun<int, int>(std::isspace))));
+    while (std::getline(input, line)) {
+        // Remove white space from the beginning of the string.
+        line.erase(line.begin(),
+                   std::find_if(line.begin(), line.end(),
+                                [](unsigned char character) -> bool {
+                                    return !std::isspace(character);
+                                }));
 
-		// If the line of the file begins with '#', skip it.
-		if (line[0] == '#') {
-			continue;
-		}
+        // If the line of the file begins with '#', skip it.
+        if (line[0] == '#') {
+            continue;
+        }
 
-		// Read the comma-delimited source file
-		std::string data;
-		std::stringstream linestream(line);
+        // Read the comma-delimited source file
+        std::string data;
+        std::stringstream linestream(line);
 
-		unsigned int count = 0;
-		T value;
-		while (std::getline(linestream, data, ',')) {
-			std::stringstream(data) >> value;
-			if (count == 0) {
-				time_.push_back(value);
-			} else {
-				rate_.push_back(value);
-			}
+        unsigned int count = 0;
+        T value;
+        while (std::getline(linestream, data, ',')) {
+            std::stringstream(data) >> value;
+            if (count == 0) {
+                time_.push_back(value);
+            } else {
+                rate_.push_back(value);
+            }
 
-			count++;
-		}
-	}
+            count++;
+        }
+    }
 }
 
-template<class T>
-void PointSource<T>::Scale(const T scalar) {
-	for (unsigned int i = 0; i < time_.size(); i++) {
-		rate_[i] *= scalar;
-	}
+template <class T> void PointSource<T>::Scale(T scalar) {
+    for (unsigned int i = 0; i < time_.size(); i++) {
+        rate_[i] *= scalar;
+    }
 }
 
-template<class T>
-T PointSource<T>::interpolated_rate(const T current_time) const {
-	T rate_i = (T)0;
-	// TODO: Make this linear interpolation faster. The for loop can become
-	// computationally expensive if the number of time steps is large.
-	for (unsigned int i = 0; i < time_.size()-1; i++) {
-		if (current_time >= time_[i] && current_time < time_[i+1]) {
-			rate_i = rate_[i] + (rate_[i+1] - rate_[i]) / (time_[i+1] - time_[i]) *
-			                    (current_time - time_[i]);
-			break;
-		}
-	}
+template <class T>
+auto PointSource<T>::interpolated_rate(T current_time) const -> T {
+    T rate_i = (T)0;
+    // TODO: Make this linear interpolation faster. The for loop can become
+    // computationally expensive if the number of time steps is large.
+    for (unsigned int i = 0; i < time_.size() - 1; i++) {
+        if (current_time >= time_[i] && current_time < time_[i + 1]) {
+            rate_i = rate_[i] + (rate_[i + 1] - rate_[i]) /
+                                    (time_[i + 1] - time_[i]) *
+                                    (current_time - time_[i]);
+            break;
+        }
+    }
 
-	return rate_i;
+    return rate_i;
 }
