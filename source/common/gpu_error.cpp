1ce1f7fef2b677d295b536c6f18880a1be63c428
Byron Tasseff <byron@tasseff.com>
2025-05-25 01:01:21 +0000

    Clean up GPU error handling.

--- source/common/gpu_error.cpp
+++ source/common/gpu_error.cpp
@@ -1,14 +1,15 @@
 #ifdef __CUDACC__
 
-#include "error.h"
 #include "gpu_error.h"
+#include "error.h"
 
 void GpuAssert(cudaError_t code, std::string file, int line) {
-	if (code != cudaSuccess) {
-		std::string error_string = cudaGetErrorString(code) + std::string(" ") +
-		                           file + std::string(" ") + std::to_string(line);
-		PrintErrorAndExit(error_string);
-	}
+    if (code != cudaSuccess) {
+        std::string error_string = cudaGetErrorString(code) + std::string(" ") +
+                                   file + std::string(" ") +
+                                   std::to_string(line);
+        PrintErrorAndExit(error_string);
+    }
 }
 
 #endif
--- source/common/gpu_error.h
+++ source/common/gpu_error.h
@@ -2,11 +2,14 @@
 
 #ifdef __CUDACC__
 
-#include <string>
 #include <cuda.h>
 #include <cuda_runtime.h>
+#include <string>
 
-#define GpuErrChk(ans) { GpuAssert((ans), __FILE__, __LINE__); }
+#define GpuErrChk(ans)                                                         \
+    {                                                                          \
+        GpuAssert((ans), __FILE__, __LINE__);                                  \
+    }
 void GpuAssert(cudaError_t code, std::string file, int line);
 
 #endif
